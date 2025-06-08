ecfc90a56c44559b2d5eb5bcd6cbb2c81d0db4c7
BHWeston <ben.hobbs@weston.ac.uk>
2025-06-08 20:47:16 +0100

    Variables and output code inputted

---
+++ 01- Variables and Output/index.php
@@ -0,0 +1,21 @@
+<?php
+// ^ starting PHP tag
+
+// To output details within PHP, this will make use of the "echo" keyword, similar to functions in Python, echo will used () to indicate what will be included. For instance:
+
+echo("Hello World");
+
+// You will notice with the above, that there is a semi-colon at the end. This determines the end of a line in PHP; note that this is not required for the final line within PHP.
+
+// In order to create variables within PHP, this will make use of the $ symbol, similar to Python, variables to not need to have a data type assigned, so we can create the following:
+
+$exampleVariable = "Hello World";
+
+// To echo this out, it will look like the following:
+echo($exampleVariable);
+
+// If you are wanting to concatenate commands together, this will look like the following:
+echo("The variable contains the following " .$exampleVariable);
+
+// v ending PHP tag
+?>
