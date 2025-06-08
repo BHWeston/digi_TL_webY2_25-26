9ff43a0f900377e59d1f06b86c8a88c65fe3ab13
BHWeston <ben.hobbs@weston.ac.uk>
2025-06-08 21:06:04 +0100

    Setup of constants example

---
+++ 01- Variables and Output/constants.php
@@ -0,0 +1,10 @@
+<?php
+    /* Constants do not change through-out the project. There are two methods to create a variable that does not change:
+    1. define()
+    2. const
+    */
+
+    // define("constname", "value");
+
+    // const var = "value"
+?>
