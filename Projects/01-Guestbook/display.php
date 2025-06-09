7ab2e54dee705c65e91de1c73d715d7d533998f6
BHWeston <ben.hobbs@weston.ac.uk>
2025-06-09 11:41:29 +0100

    Fundamentals of programme to complete the challenge

---
+++ Projects/01-Guestbook/display.php
@@ -0,0 +1,7 @@
+<?php
+    $sEnteredName = $_POST['sName'];
+    $sEnteredMessage = $_POST['sMessage'];
+
+    echo("This is the entered name: <br>" .$sEnteredName."<br>");
+    echo("This is the entered message: <br>".$sEnteredMessage);
+?>
