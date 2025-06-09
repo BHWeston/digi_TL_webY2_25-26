896dd9cb8f763604ec485a29d28af02304f10f30
BHWeston <ben.hobbs@weston.ac.uk>
2025-06-09 11:44:18 +0100

    Added comments for the project

--- Projects/01-Guestbook/display.php
+++ Projects/01-Guestbook/display.php
@@ -1,7 +1,9 @@
 <?php
+    // Variables for getting the data from the form.html page
     $sEnteredName = $_POST['sName'];
     $sEnteredMessage = $_POST['sMessage'];
 
+    // Outputs for the variables:
     echo("This is the entered name: <br>" .$sEnteredName."<br>");
     echo("This is the entered message: <br>".$sEnteredMessage);
 ?>
