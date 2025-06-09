0163a68e838795caff022818b2e610462b5ac14f
BHWeston <ben.hobbs@weston.ac.uk>
2025-06-09 12:05:22 +0100

    Added validation to the commands to check if values have been entered

--- Projects/01-Guestbook/display.php
+++ Projects/01-Guestbook/display.php
@@ -1,9 +1,16 @@
 <?php
-    // Variables for getting the data from the form.html page
-    $sEnteredName = $_POST['sName'];
-    $sEnteredMessage = $_POST['sMessage'];
 
-    // Outputs for the variables:
-    echo("This is the entered name: <br>" .$sEnteredName."<br>");
-    echo("This is the entered message: <br>".$sEnteredMessage);
+    // If statement to check if the user has submitted a POST request, this stops a user being able to bypass directly to the page and viewing data that has not been submitted:
+
+    // IF statements in PHP use the following syntax if(condition) { action }
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Variables for getting the data from the form.html page
+        $sEnteredName = htmlspecialchars($_POST['sName']);
+        $sEnteredMessage = htmlspecialchars($_POST['sMessage']);
+
+        // Outputs for the variables:
+        echo("This is the entered name: <br>" .$sEnteredName."<br>");
+        echo("This is the entered message: <br>".$sEnteredMessage);
+
+    }
 ?>
