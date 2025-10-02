b60f9c4f7499bd4ad7e7e8b0ef19ad86745ab056
BHWeston <ben.hobbs@weston.ac.uk>
2025-10-02 11:23:39 +0100

    Updated files 02 10 2025

---
+++ Lessons/01 - PHP ReIntro/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Strings PHP</title>
+</head>
+<body>
+    <?php
+        // Types of strings in PHP:
+        $my_var = "World";
+        echo("Hello $my_var"); // Double Quotes - Outputs "Hello World"
+        echo('Hello $my_var'); // Literal - Outputs Hello $my_var
+        echo(var_dump($my_var));
+        echo("Hello"*2);
+    ?>
+</body>
+</html>
