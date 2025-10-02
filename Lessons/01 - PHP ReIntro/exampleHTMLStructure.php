8b70d7f2a05a271a779f1300b806dd4ae9727e0b
BHWeston <ben.hobbs@weston.ac.uk>
2025-10-02 11:39:16 +0100

    Updated example HTML structure

---
+++ Lessons/01 - PHP ReIntro/exampleHTMLStructure.php
@@ -0,0 +1,19 @@
+<!-- Defines the document type (being HTML) -->
+<!DOCTYPE html>
+
+<!-- Identifies the following code is HTML and in English -->
+<html lang="en">
+
+<!-- Determines the thinking behind the scenes of the code, this deals with links, meta tags and scripts-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Determines the tab name on the webpage -->
+    <title>Document</title>
+</head>
+<!-- This will contain the main contents on the website -->
+<body>
+    <!-- This will contain the content on the webpage -->
+</body>
+</html>
