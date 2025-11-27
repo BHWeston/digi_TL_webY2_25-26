e9fb642c91623039ce9075cc4e5d3fb3959bf0e8
BHWeston <ben.hobbs@weston.ac.uk>
2025-11-27 11:59:40 +0000

    Updated index page with example structure

--- Projects/RigetZoo/index.php
+++ Projects/RigetZoo/index.php
@@ -1,1 +1,31 @@
 <!-- Introductory page to showcase the zoo and features of this -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar bg-body-tertiary navbar-expand-lg">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tickets</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section class="container-fluid">
+        <section class="row">
+            <section class="col-6">Col 1</section>
+            <section class="col-3">Col 2</section>
+            <section class="col-3">Col 3</section>
+        </section>
+    </section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
