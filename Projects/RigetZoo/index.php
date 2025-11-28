1841a6d3b832d47907db3833e0955d880477e52e
BHWeston <ben.hobbs@weston.ac.uk>
2025-11-28 13:41:13 +0000

    Updated to create homepage

--- Projects/RigetZoo/index.php
+++ Projects/RigetZoo/index.php
@@ -6,23 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <nav class="navbar bg-body-tertiary navbar-expand-lg">
         <div class="container-fluid">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Tickets</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="tickets.php">Tickets</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
             </ul>
         </div>
     </nav>
-    <section class="container-fluid">
-        <section class="row">
-            <section class="col-6">Col 1</section>
-            <section class="col-3">Col 2</section>
-            <section class="col-3">Col 3</section>
-        </section>
+    <section class="container">
+        <div class="row text-center">
+            <div class="col-4">
+                <h1>Tigers</h1>
+                <img src="images/tigers.jpg" class="img-fluid" alt="Image of tigers">
+                <p>Write-up about tigers - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi iusto quidem autem harum! Vitae repudiandae debitis beatae iusto, error voluptates quas est nemo possimus omnis magnam suscipit quam dolores qui.</p>
+            </div>
+            <div class="col-4">
+                <h1>Penguins</h1>
+                <img src="images/penguins.jpg" class="img-fluid" alt="Image of penguins">
+                <p>Write-up about penguins - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi iusto quidem autem harum! Vitae repudiandae debitis beatae iusto, error voluptates quas est nemo possimus omnis magnam suscipit quam dolores qui.</p>
+            </div>
+            <div class="col-4">
+                <h1>Dogs</h1>
+                <img src="images/dogs.jpg" class="img-fluid" alt="Image of dogs">
+                <p>Write-up about dogs - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi iusto quidem autem harum! Vitae repudiandae debitis beatae iusto, error voluptates quas est nemo possimus omnis magnam suscipit quam dolores qui.</p>
+            </div>
+        </div>
     </section>
 
 
